89a797e42b13801ae2b75292dd8ab6af790ca2ec
Pasquale Voci <pa.voci@almaviva.it>
2025-09-29 12:11:04 +0200

    LevelManager

---
+++ app/src/main/java/com/game/omino/levels/LevelManager.java
@@ -0,0 +1,41 @@
+package com.game.omino.levels;
+
+import android.content.Context;
+import com.game.omino.engine.GameLoop;
+
+public class LevelManager {
+
+    private final Context context;
+    private static Level currentLevel;
+
+    public LevelManager(Context context) {
+        this.context = context;
+
+        /**
+         * gestire la selezione dei livelli
+         */
+        loadLevel("0");
+        gameLoop = new GameLoop(this, context);
+    }
+
+    public static void startLevel(){
+        gameLoop.start();
+    }
+
+    public static void stopLevel(){
+        gameLoop.stop();
+    }
+
+    public static void startNextLevel(){
+
+    }
+
+    public static Level getCurrentLevel() {
+        return currentLevel;
+    }
+
+
+    private void loadLevel(String level){
+        currentLevel = LevelLoader.loadLevelFromAsset(context, "/levels/level_"+level);
+    }
+}
